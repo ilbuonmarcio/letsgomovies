b7623e5c876018956fa4081f6a38702ece810c70
maaaybe <marciozgaming@gmail.com>
2020-02-08 10:46:52 +0100

    Fixed dispatcher typo

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-import logging
 import os
+import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -11,7 +11,8 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dispatcher = Dispatcher(bot)
 
-@dp.message_handler(commands=['start', 'help'])
+
+@dispatcher.message_handler(commands=['start', 'help'])
 async def send_cities_list(message: types.Message):
     await message.reply("Hi, LetsGoMovies here!\nSelect your city below:")
 
